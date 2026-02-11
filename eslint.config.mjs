b37ed34d588d2ccf323c669ae6a7bd2c0c99f74c
4n994 <anggasatrya007@gmail.com>
2026-02-11 14:21:19 +0700

    instalasi nextjs 15 dan setting eslint beberapa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,17 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  rules: {
+    // untuk menangani dependecies yang sekiranya tidak dibutuhkan
+    "react-hooks/exhautive-deps": "off",
+    // komponen jauh lebih unique, beberapa fitur dari shadcn ui terkadang ada hal hala beberapa yang mengganggu kalau mengaktifkan jsx-key
+    "react/jsx-key": "off",
+    // tidak memakakai type any pada typescript sebenarnya cuma untuk midtrans tapi aku nonaktifkan
+    "@typescript-eslint/no-explicit-any": "off",
+    // dimatikan agar bisa menghilangkan sesuatu dari sebuiah objek
+    "typescript-eslint/no-unused-vars": "off",
+  },
+});
+
 export default eslintConfig;
